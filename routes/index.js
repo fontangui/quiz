58fc9f5d1461bdde5fb6945c6f68adb0b756f253
davideandres95 <davideandres95@gmail.com>
2016-04-26 20:16:04 +0200

    ready heroku

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz', github:'<a href="https://github.com/davideandres95/quiz2">Proyecto en github</a>'});
 });
 
-<<<<<<< HEAD
-
-=======
 router.get('/author', function(req, res, next){
 	res.render('author', { github:'<a href="https://github.com/davideandres95/quiz2">Proyecto en github</a>',
 		title: 'Quiz', autores: 'David de Andrés y Diego Martín Crespo',
@@ -19,6 +16,5 @@ router.get('/author', function(req, res, next){
 		foto2: '<img src="../public/images/diegomartin.jpg" alt="Diego Martín">'
 	})
 });
->>>>>>> origin/master
 
 module.exports = router;
