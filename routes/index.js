664cc277000033a5f3e51767b7af99f23fc7e469
fontangui <javifontangui@gmail.com>
2016-04-27 23:33:54 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,16 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz', github:'<a href="https://github.com/davideandres95/quiz2">Proyecto en github</a>'});
+  res.render('index', { title: 'Quiz', github:'<a href="https://github.com/fontangui/quiz">Proyecto en github</a>'});
 });
 
 router.get('/author', function(req, res, next){
-	res.render('author', { github:'<a href="https://github.com/davideandres95/quiz2">Proyecto en github</a>',
-		title: 'Quiz', autores: 'David de Andrés y Diego Martín Crespo',
-		video1:'<iframe width="560" height="315" src="https://www.youtube.com/embed/jO2ecDT6Vgw" frameborder="0" allowfullscreen></iframe>',
-		video2: '<iframe width="560" height="315" src="https://www.youtube.com/embed/iCme0HRgd0U" frameborder="0" allowfullscreen></iframe>',
-		foto1: '<img src="/images/Davideandres.jpg" alt="David de Andres">',
-		foto2: '<img src="/images/diegomartin.jpg" alt="Diego Martín" style="width:200px; height:200px">'
+	res.render('author', { github:'<a href="https://github.com/fontangui/quiz">Proyecto en github</a>',
+		title: 'Quiz', autores: 'Javier Fontán : @fontangui',
+		video1:'<iframe width="560" height="315" src="https://www.youtube.com/watch?v=NBB8PDenGJM" frameborder="0" allowfullscreen></iframe>',
+
+		foto1: '<img src="/images/javier.jpg" alt="Club Deportivo Etsit">',
+		foto2: '<img src="/images/satelec.jpg" alt="Satelec 2016" style="width:200px; height:200px">
 	})
 });
 
